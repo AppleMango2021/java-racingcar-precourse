583743de47f288d93786fafc4be0ea47cb737188
JoyceLee9310 <leejoyce9310@gmail.com>
2021-05-02 19:34:07 +0900

    feat : 대회를 개최하여 진행하고, 우승자를 발표하는 기능

---
+++ src/main/java/Contest.java
@@ -0,0 +1,53 @@
+import java.util.List;
+
+public class Contest {
+
+	private Race race;
+	private Cars cars;
+	private List<Car> winners;
+	private InputView inputView;
+	private OutputView outputView;
+
+	public Contest() {
+		this.inputView = new InputView();
+		this.outputView = new OutputView();
+	}
+
+	public void open() {
+		if (checkCars() && checkNumber()) {
+			outputView.printMessage(System.lineSeparator() + Message.NOTICE_BEFORE_PRINT_RESULT.showText());
+			race.start();
+			winners = cars.announceWinners();
+			outputView.printMessage(NameUtils.listCarToStringName(winners));
+		}
+	}
+
+	private boolean checkNumber() {
+		outputView.printMessage(Message.REQUEST_INPUT_TOTAL_ROUND);
+		return roundDecided(inputView.returnNumber());
+	}
+
+	private boolean checkCars() {
+		boolean carsPrepared = false;
+		outputView.printMessage(Message.REQUEST_INPUT_CARS_NAME);
+		while (!carsPrepared) {
+			carsPrepared = carsRegistered(inputView.returnCarNames());
+		}
+		return carsPrepared;
+	}
+
+	boolean roundDecided(int number) {
+		race = new Race(number, cars);
+		return true;
+	}
+
+	boolean carsRegistered(List<String> names) {
+		try {
+			cars = new Cars(names);
+		} catch (InvalidInputException e) {
+			outputView.printMessage(e.getMessage());
+			return false;
+		}
+		return true;
+	}
+}
